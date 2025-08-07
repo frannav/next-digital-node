8db9fbbbc2998a24673df743bf1cdfcd5f41cb47
frannav <frannavmart@gmail.com>
2025-08-07 14:48:17 +0100

    Add swagger for withdrawal module

--- src/modules/withdrawal/withdrawal.router.ts
+++ src/modules/withdrawal/withdrawal.router.ts
@@ -5,6 +5,25 @@ import { createWithdrawalSchema } from "./withdrawal.schema.ts";
 
 const router = Router();
 
+/**
+ * @openapi
+ * /api/withdrawals:
+ *   post:
+ *     tags:
+ *       - Withdrawals
+ *     summary: Create a new withdrawal
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateWithdrawalInput'
+ *     responses:
+ *       201:
+ *         description: Withdrawal created successfully
+ *       400:
+ *         description: Bad request
+ */
 router.post(
 	"/",
 	validateRequest(createWithdrawalSchema),
--- src/modules/withdrawal/withdrawal.schema.ts
+++ src/modules/withdrawal/withdrawal.schema.ts
@@ -1,5 +1,23 @@
 import { z } from "zod";
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     CreateWithdrawalInput:
+ *       type: object
+ *       required:
+ *         - cardId
+ *         - amount
+ *       properties:
+ *         cardId:
+ *           type: string
+ *         amount:
+ *           type: number
+ *         atmBankId:
+ *           type: string
+ */
+
 export const createWithdrawalSchema = z.object({
 	body: z.object({
 		cardId: z.string(),
