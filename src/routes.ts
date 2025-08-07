daad5aa286e7902cf7ade11ec114b458616dc0f7
frannav <frannavmart@gmail.com>
2025-08-07 14:30:12 +0100

    Fix lint errors

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,19 @@
 import { Router } from "express";
-import accountRouter from "./modules/account/account.router.ts";
-import cardRouter from "./modules/card/card.router.ts";
-import depositRouter from "./modules/deposit/deposit.router.ts";
-import transferRouter from "./modules/transfer/transfer.router.ts";
-import userRouter from "./modules/user/user.router.ts";
-import withdrawalRouter from "./modules/withdrawal/withdrawal.router.ts";
+import accountRouter from "./modules/account/account.router";
+import cardRouter from "./modules/card/card.router";
+import depositRouter from "./modules/deposit/deposit.router";
+import movementRouter from "./modules/movement/movement.router";
+import transferRouter from "./modules/transfer/transfer.router";
+import userRouter from "./modules/user/user.router";
+import withdrawalRouter from "./modules/withdrawal/withdrawal.router";
 
 const router: Router = Router();
 
 router.use("/api/users", userRouter);
 router.use("/api/accounts", accountRouter);
 router.use("/api/cards", cardRouter);
-// Nested route for movements under accounts
-// TODO: error with movements route fix when finish mvp
-// accountRouter.use('/:accountId/movements', movementRouter);
+router.use("/api/movements", movementRouter);
 router.use("/api/withdrawals", withdrawalRouter);
 router.use("/api/deposits", depositRouter);
 router.use("/api/transfers", transferRouter);
-
 export default router;
