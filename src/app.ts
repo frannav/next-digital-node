a01f6a7c5dec4539e0468501f8c5c5206ac72b99
frannav <frannavmart@gmail.com>
2025-08-07 14:47:01 +0100

    Add swagger config

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import type { Express } from "express";
 import express from "express";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './config/swagger';
 import { errorHandler } from "./middleware/errorHandler";
 import { requestLogger } from "./middleware/pino";
 import routes from "./routes";
@@ -10,6 +12,8 @@ const app: Express = express();
 app.use(requestLogger);
 app.use(express.json());
 app.use(cors());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(routes);
 
---
+++ src/config/swagger.ts
@@ -0,0 +1,23 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const swaggerOptions: swaggerJSDoc.Options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Node JS API',
+      version: '1.0.0',
+      description: 'API for the Node JS project',
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT || 3000}`,
+      },
+    ],
+  },
+  apis: ['./src/modules/**/*.ts', './src/routes.ts'],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+export default swaggerDocs;
+
