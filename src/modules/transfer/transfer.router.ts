ed03346ee5351794b24ff16826ab67329ab0c2df
frannav <frannavmart@gmail.com>
2025-08-07 14:48:01 +0100

    Add swagger for transfer module

--- src/modules/transfer/transfer.router.ts
+++ src/modules/transfer/transfer.router.ts
@@ -5,6 +5,25 @@ import { createTransferSchema } from "./transfer.schema.ts";
 
 const router: Router = Router();
 
+/**
+ * @openapi
+ * /api/transfers:
+ *   post:
+ *     tags:
+ *       - Transfers
+ *     summary: Create a new transfer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateTransferInput'
+ *     responses:
+ *       201:
+ *         description: Transfer created successfully
+ *       400:
+ *         description: Bad request
+ */
 router.post(
 	"/",
 	validateRequest(createTransferSchema),
--- src/modules/transfer/transfer.schema.ts
+++ src/modules/transfer/transfer.schema.ts
@@ -1,6 +1,25 @@
 import IBAN from "iban";
 import { z } from "zod";
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     CreateTransferInput:
+ *       type: object
+ *       required:
+ *         - fromAccountId
+ *         - toIban
+ *         - amount
+ *       properties:
+ *         fromAccountId:
+ *           type: string
+ *         toIban:
+ *           type: string
+ *         amount:
+ *           type: number
+ */
+
 export const createTransferSchema = z.object({
 	body: z.object({
 		fromAccountId: z.string(),
