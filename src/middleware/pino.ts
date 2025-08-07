b7b213159c7a782dace7f7464c034d0a68d3ca08
frannav <frannavmart@gmail.com>
2025-08-07 12:53:54 +0100

    Add pino logger

---
+++ src/middleware/pino.ts
@@ -0,0 +1,16 @@
+import pinoHttp from 'pino-http';
+import pino from 'pino';
+
+const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
+      ignore: 'pid,hostname',
+    },
+  },
+});
+
+export const requestLogger = pinoHttp({ logger });
+
