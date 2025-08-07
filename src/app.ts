a3eb8bee65d85446a08fa4b325fb018d162c2b54
frannav <frannavmart@gmail.com>
2025-08-07 14:50:33 +0100

    Biome format

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import cors from "cors";
 import type { Express } from "express";
 import express from "express";
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocs from './config/swagger';
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./config/swagger";
 import { errorHandler } from "./middleware/errorHandler";
 import { requestLogger } from "./middleware/pino";
 import routes from "./routes";
@@ -13,7 +13,7 @@ app.use(requestLogger);
 app.use(express.json());
 app.use(cors());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(routes);
 
